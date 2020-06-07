6e975ea25ea51b0d66e309fb64e257cfe3d6dbb6
navin <navinsourie@gmail.com>
2020-06-06 23:21:44 -0500

    file changes as per andy app

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,26 @@
-console.log("client side javascript is loaded!!!");
+console.log('Client side javascript file is loaded!')
 
-// fetch('https://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
-const weatherForm = document.querySelector("form");
-const loc = document.querySelector("input");
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
 
 weatherForm.addEventListener("submit", event => {
   event.preventDefault();
-  const address = loc.value;
+  const address = search.value;
   console.log("form submitted::", address);
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
 
   fetch(`/weather?address=${address}`).then(response => {
     response.json().then(data => {
       console.log(JSON.stringify(data));
       if (data.error) {
         console.log(data.error);
+        messageOne.textContent = data.error
       } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
         console.log(data.location);
         console.log(data.forecast);
       }
